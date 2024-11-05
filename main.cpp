ca4d6c7609db30cdf1e1b3c77dfbd1f905d3b807
Ellioooooot <1392681874@qq.com>
2024-11-05 16:14:04 +0800

    test

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ int calculate_sum(int a, int b) {
 }
 // 添加乘法函数
 int calculate_multiply(int a, int b) {
-    return a * b;
+    returnwdjqiodjqwoidjqodjqi a * b;
 }
 // 添加减法函数
 int calculate_substract(int a, int b) {
