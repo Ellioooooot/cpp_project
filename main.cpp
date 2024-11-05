f550f91a586c9cb8b5d83cdb5131fe522bd46de4
Davinci <1152025734@qq.com>
2024-11-05 10:41:29 +0800

    Add division function

--- main.cpp
+++ main.cpp
@@ -13,14 +13,23 @@ int calculate_multiply(int a, int b) {
 int calculate_substract(int a, int b) {
     return a - b;
 }
+// 添加除法函数
+int calculate_substract(int a, int b) {
+    if(b==0){
+	    return 0;
+    }
+    else
+	    return a/b;
+}
+
 
 int main() {
     int num1, num2;
     cout << "请输入两个数字：" << endl;
     cin >> num1 >> num2;
 
-    cout << "和为：" << calculate_sum(num1, num2) << endl;
-    cout << "积为：" << calculate_multiply(num1, num2) << endl;
-    cout << "差为：" << calculate_substract(num1, num2) << endl;
+    cout << "其和为：" << calculate_sum(num1, num2) << endl;
+    cout << "其积为：" << calculate_multiply(num1, num2) << endl;
+    cout << "其差为：" << calculate_substract(num1, num2) << endl;
     return 0;
 }
